f1b68d182dcaf149723787920dd7466d0287d340
KoizumiGensuke <53796235+KoizumiGensuke@users.noreply.github.com>
2021-02-21 15:36:05 +0900

    fix order of fields

--- SaveLog.py
+++ SaveLog.py
@@ -6,18 +6,22 @@ import csv
 import datetime
 import humax # https://github.com/radomirbosak/humax
 import os
-import locale # to avoid strftime locale error
+#import locale # to avoid strftime locale error
 
-h = humax.Humax('http://192.168.0.1')
-h.login('id', 'password')
+h = humax.Humax('http://192.168.0.1') # modem address
+h.login('admin', 'password')          # id / password
 log = h.post(humax.Device.getDBInfo, token=h.token, return_raw=True, CM_SNMP_Event_Log='')
 entries = log['result']['CM_SNMP_Event_Log']['snmp_event_log']
+
+# store data to csv file
 folder = os.path.dirname(os.path.abspath(__file__))
-locale.setlocale(locale.LC_CTYPE, 'Japanese_Japan.932') # to avoid strftime locale error
+#locale.setlocale(locale.LC_CTYPE, 'Japanese_Japan.932') # to avoid strftime locale error
 csvfilename = datetime.datetime.strftime(datetime.datetime.now(), folder + '/Humax%Y%m%d%H%M%S.csv')
 
 with open(csvfilename, 'w') as fout:
-    writer = csv.DictWriter(fout, fieldnames = list(entries[0].keys()))
+    fields = list(entries[0].keys())
+    fields.sort(reverse=True) # expecting ['time', 'priority', 'description']
+    writer = csv.DictWriter(fout, fieldnames = fields)
     writer.writeheader()
     for entry in entries:
         entry['time'] = entry['time'][:-1] # omit tailing newline
